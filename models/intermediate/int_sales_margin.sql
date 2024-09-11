2e49ca2d5bffa27a4b9313e44dfeeb1d5e8caa4e
Anuprita Zarkar <anuprita_satpute@yahoo.com>
2024-09-11 15:46:50 +0000

    added macros in int sales margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,10 @@ select
     quantity,
     purchase_price,
     (quantity*purchase_price) as purchase_cost,
-    (revenue - (quantity * purchase_price)) as margin
+    (revenue - (quantity * purchase_price)) as margin,
+    {% macro margin_percent(revenue, purchase_cost)%}
+    safe_divide((revenue-purchase_cost),revenue) as margin_percent
+{%endmacro%}
 from {{("dbt_anu.stg_raw__sales")}}
 left join {{("dbt_anu.stg_raw__product")}}
 using (products_id)
